73ac38235bdfb0889d4f0b1f84eed70ad1de3d43
Yukai Wang <58201273+kew6688@users.noreply.github.com>
2025-08-22 09:28:51 +0800

    [Fix] dataset depth shape mismatch (#36)
    * fix dataset depth shape mismatch
    
    * use shape

--- internnav/dataset/base.py
+++ internnav/dataset/base.py
@@ -52,7 +52,9 @@ class BaseDataset(IterableDataset):
 
         # Handle RGB and depth features/data
         new_data['rgb'] = data['camera_info'][self.camera_name]['rgb']
-        if len(data['camera_info'][self.camera_name]['depth']) == 2:
+        depth_shape = data['camera_info'][self.camera_name]['depth'].shape  # empty data is filtered
+        if len(depth_shape) == 3:
+            # [N, 256, 256] -> [N, 256, 256, 1]
             new_data['depth'] = np.expand_dims(data['camera_info'][self.camera_name]['depth'], axis=-1)
         else:
             new_data['depth'] = data['camera_info'][self.camera_name]['depth']
